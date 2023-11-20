f305aff96f44932bf840a8d785a612b6056f7e74
Christian Moyes <moyechri846@gmail.com>
2023-11-20 10:43:25 -0500

    Back animation done

--- Game1.cs
+++ Game1.cs
@@ -10,17 +10,21 @@ namespace Animation_Summative
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
         Texture2D spaceTexture;
-        Texture2D tieFighterTexture;
         Texture2D xWingTexture;
+        Texture2D tieFighterTexture;
         Texture2D blastTexture;
         Texture2D explosionTexture;
-
-        List<Rectangle> space = new List<Rectangle>();
+        Rectangle space1Rect, space2Rect;
+        Rectangle xWingRect;
+        Rectangle tieFighterRect;
+        Rectangle blastRect;
+        Vector2 spaceSpeed;
+        float time;
 
         enum Screen
         {
             Intro,
-            Something,
+            Battle,
             End
         }
         Screen screen;
@@ -38,13 +42,12 @@ namespace Animation_Summative
 
         protected override void Initialize()
         {
-            screen = Screen.Intro;
-            spaceTexture = Content.Load<Texture2D>("Space");
-            tieFighterTexture = Content.Load<Texture2D>("Tie");
-            xWingTexture = Content.Load<Texture2D>("X-Wing");
-            blastTexture = Content.Load<Texture2D>("rectangle");
-            explosionTexture = Content.Load<Texture2D>("PixelBoom");
-
+            screen = Screen.Battle;
+            
+            space1Rect = new Rectangle(0, 0, 800, 600);
+            space2Rect = new Rectangle(0, -600, 800, 600);
+            xWingRect = new Rectangle(350, 475, 100, 100);
+            spaceSpeed = new Vector2(0, 2);
 
 
             base.Initialize();
@@ -53,14 +56,32 @@ namespace Animation_Summative
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            spaceTexture = Content.Load<Texture2D>("Space");
+            tieFighterTexture = Content.Load<Texture2D>("Tie");
+            xWingTexture = Content.Load<Texture2D>("X-Wing");
+            blastTexture = Content.Load<Texture2D>("rectangle");
+            explosionTexture = Content.Load<Texture2D>("PixelBoom");
 
         }
 
         protected override void Update(GameTime gameTime)
         {
+            time = (float)gameTime.TotalGameTime.TotalSeconds;
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            space1Rect.Y += (int)spaceSpeed.Y;
+            if (space1Rect.Top >= graphics.PreferredBackBufferHeight)
+            {
+                space1Rect.Y = -600;
+            }
+
+            space2Rect.Y += (int)spaceSpeed.Y;
+            if (space2Rect.Top >= graphics.PreferredBackBufferHeight)
+            {
+                space2Rect.Y = -600;
+            }
 
 
             base.Update(gameTime);
@@ -68,8 +89,24 @@ namespace Animation_Summative
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
             spriteBatch.Begin();
+            if (screen == Screen.Intro)
+            {
+
+            }
+            else if (screen == Screen.Battle)
+            {
+                spriteBatch.Draw(spaceTexture, space1Rect, Color.White);
+                spriteBatch.Draw(spaceTexture, space2Rect, Color.White);
+                spriteBatch.Draw(xWingTexture, xWingRect, Color.White);
+
+            }
+            else if (screen == Screen.End)
+            {
+
+            }
+
             spriteBatch.End();
 
             base.Draw(gameTime);
