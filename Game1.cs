421d041112d1254d68e6e9d78c620ff02d6c1305
Christian Moyes <moyechri846@gmail.com>
2023-11-21 10:46:20 -0500

    First Blast Made!

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 
 namespace Animation_Summative
@@ -8,6 +9,7 @@ namespace Animation_Summative
     public class Game1 : Game
     {
         double introTime; // When user switches to battle screen, save current time elapsed to time animations
+        bool redBlast1, redBlast2, greenBlast1, greenBlast2;
 
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
@@ -19,8 +21,12 @@ namespace Animation_Summative
         Rectangle space1Rect, space2Rect;
         Rectangle xWingRect;
         Rectangle tieFighterRect;
-        Rectangle blastRect;
+        Rectangle redBlastRect1, redBlastRect2, greenBlastRect1, greenBlastRect2;
         Vector2 spaceSpeed;
+        Vector2 xWingSpeed;
+        Vector2 tieSpeed;
+        Vector2 redBlastSpeed1, redBlastSpeed2, greenBlastSpeed1, greenBlastSpeed2;
+        float runTime;
         float timeTotal;
         float timeInIntro;
 
@@ -46,11 +52,24 @@ namespace Animation_Summative
         protected override void Initialize()
         {
             screen = Screen.Battle;
-            
+
+            redBlast1 = false;
+            redBlast2 = false;
+            greenBlast1 = false;
+            greenBlast2 = false;
+
             space1Rect = new Rectangle(0, 0, 800, 600);
             space2Rect = new Rectangle(0, -600, 800, 600);
             xWingRect = new Rectangle(350, 475, 100, 100);
+            tieFighterRect = new Rectangle(0, 0, 100, 100);
+            redBlastRect1 = new Rectangle(650, 500, 15, 35);
+            redBlastRect2 = new Rectangle(0, 0, 15, 35);
+            greenBlastRect1 = new Rectangle(0, 0, 15, 35);
+            greenBlastRect2 = new Rectangle(0, 0, 15, 35);
             spaceSpeed = new Vector2(0, 2);
+            xWingSpeed = new Vector2(0, 0);
+            tieSpeed = new Vector2(0, 0);
+            redBlastSpeed1 = new Vector2(0, -4);
 
 
             base.Initialize();
@@ -70,23 +89,76 @@ namespace Animation_Summative
         protected override void Update(GameTime gameTime)
         {
 
-            timeTotal = (float)gameTime.TotalGameTime.TotalSeconds;
+            runTime = (float)gameTime.TotalGameTime.TotalSeconds;
 
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            space1Rect.Y += (int)spaceSpeed.Y;
-            if (space1Rect.Top >= graphics.PreferredBackBufferHeight)
+            if (screen == Screen.Battle)
             {
-                space1Rect.Y = -600;
-            }
+                timeInIntro = 0;
+                timeTotal = runTime - timeInIntro;
 
-            space2Rect.Y += (int)spaceSpeed.Y;
-            if (space2Rect.Top >= graphics.PreferredBackBufferHeight)
-            {
-                space2Rect.Y = -600;
-            }
+                space1Rect.Y += (int)spaceSpeed.Y;
+                if (space1Rect.Top >= graphics.PreferredBackBufferHeight)
+                {
+                    space1Rect.Y = -600;
+                }
 
+                space2Rect.Y += (int)spaceSpeed.Y;
+                if (space2Rect.Top >= graphics.PreferredBackBufferHeight)
+                {
+                    space2Rect.Y = -600;
+                }
+
+                // Checking if Blasts collide or leave
+
+                if (redBlastRect1.Bottom == 0)
+                {
+                    redBlast1 = false;
+                }
+
+
+
+
+                // Changing speeds and adding effects based on time
+
+                if (timeTotal >= 5 && timeTotal < 9)
+                {
+                    xWingSpeed.X = 1;
+                }
+                else if (timeTotal > 9)
+                {
+                    xWingSpeed.X = 0;
+                }
+
+                if ( Math.Round(timeTotal) == 9 )
+                {
+                    redBlast1 = true;
+                }
+                
+                // Applying speeds
+
+                xWingRect.X += (int)xWingSpeed.X;
+                xWingRect.Y += (int)xWingSpeed.Y;
+
+                if (redBlast1)
+                {
+                    redBlastRect1.Y += (int)redBlastSpeed1.Y;
+                }
+                if (redBlast2)
+                {
+                    redBlastRect2.Y += (int)redBlastSpeed2.Y;
+                }
+                if (greenBlast1)
+                {
+                    greenBlastRect1.Y += (int)greenBlastSpeed1.Y;
+                }
+                if (redBlast2)
+                {
+                    greenBlastRect2.Y += (int)greenBlastSpeed2.Y;
+                }
+
+            }
 
             base.Update(gameTime);
         }
@@ -104,6 +176,23 @@ namespace Animation_Summative
                 spriteBatch.Draw(spaceTexture, space1Rect, Color.White);
                 spriteBatch.Draw(spaceTexture, space2Rect, Color.White);
                 spriteBatch.Draw(xWingTexture, xWingRect, Color.White);
+
+                if (redBlast1 == true)
+                {
+                    spriteBatch.Draw(blastTexture, redBlastRect1, Color.Red);
+                }
+                if (redBlast2 == true)
+                {
+
+                }
+                if (greenBlast1 == true)
+                {
+
+                }
+                if (greenBlast2 == true)
+                {
+
+                }
 
             }
             else if (screen == Screen.End)
